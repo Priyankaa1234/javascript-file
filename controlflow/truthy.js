0d735076513aba01e37ec033df19d22bef4c5f9d
Priyanka Kumbhar <piyaa142004@gmail.com>
2025-02-01 10:10:38 +0000

    truthy,falsy

---
+++ controlflow/truthy.js
@@ -0,0 +1,52 @@
+//assuming the value is true or false w/o checking
+
+// const name="priyanka"
+// if (name) {
+//     console.log(`Name found`);
+    
+// }
+// else{
+//     console.log(`Name not found`);
+// }
+
+
+
+//here empty string is assumed as false
+// const name=""
+// if (name) {
+//     console.log(`Name found`);
+    
+// }
+// else{
+//     console.log(`Name not found`);
+// }
+
+
+
+
+//rules for truthy and falsy
+// for falsy = false,0,-0,BigInt 0n,"",null,undefined,NaN
+// for truthy = "0" ,'false', "_", [] , {} , function(){} ,
+
+
+
+//nullish coalescing operator
+let val1;
+// val1= 5 ?? 10
+// val1 = null ?? 10
+// val1= undefined ?? 14
+//val1 = null ?? 10 ?? 15
+
+//console.log(val1);
+
+
+
+//terniary operator
+//conditon ? true : false
+
+const icePrice=100
+
+icePrice>= 90 ? console.log(`not affordable`):console.log(`Affordable`)
+
+
+
