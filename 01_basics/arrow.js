304221b75fd3a61fc41bfded06a358c45b5a5e32
Priyanka Kumbhar <piyaa142004@gmail.com>
2025-07-19 17:51:27 +0000

    IIfe

--- 01_basics/arrow.js
+++ 01_basics/arrow.js
@@ -1,5 +1,6 @@
 // .this is use to refer current context , when we use it we say take the current data
-//value of this is window in browser , and here it is empty object 
+//value of 'this' is window in browser , and here it is empty object 
+// 'this' cannot be use in function , works in object
 const user= {
     username:"piyu",
     id:67523,
@@ -20,7 +21,7 @@ const user= {
 
 
            //********arrow function**********
-// const addtwo = (num1 , num2) =>{
+// const addtwo = (num1 , num2) => {
 //     return num1+num2
 
 // }
@@ -29,3 +30,12 @@ const addtwo = (num1 , num2) => (num1+num2)     //implicit return
 
 
 console.log (addtwo(5,5));
+
+
+
+//to remove the pollution or prb cause by the global variable we use IIFE
+
+(function chai(){
+    console.log(`DB connected`);
+    
+})();                                 //wrap under paranthesis and then invoke
